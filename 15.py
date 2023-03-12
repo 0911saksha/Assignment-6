95f204d33d05cf6ae1f304b6e56524907c87917c
0911saksha <108914640+0911saksha@users.noreply.github.com>
2023-03-12 14:09:36 +0530

    Add files via upload

---
+++ 15.py
@@ -0,0 +1,24 @@
+count = 0 
+sum = 0 
+product = 1 
+
+# Loop will run infinitely till user presses 'q'
+while True:
+    # Uncomment user_input = comp_input, Comment user_input = input(...) for magic
+    # user_input = comp_input
+    user_input = input('Enter a number: ')
+    if user_input == ‘q’: break
+    user_input = int(user_input)
+
+    # Store the result of addition of prev sum and user input
+    sum = sum + user_input
+        
+    # Store the result of addition of prev sum and user input
+    product = product * user_input
+
+    # Count the number of times user gave integer input
+    count = count + 1
+
+print('Average:', sum/count)
+print('Product:', product)
+ 
